c4062debffad5faba8d549515c2832cc827df562
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-13 14:36:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,8 @@ def position_taken?(board, index)
       false
     elsif board[index] != " " || board[index] == ""
       true
+    elsif board[index] == nil
+      false
     else
       false
     end
